203f9223662a9bc3b24f7a6d0ece20c90ea2f0f9
HIENLTH <hyhien@gmail.com>
2019-04-20 08:59:12 +0700

    Show image using Picasso

---
+++ app/src/main/java/info/hienlth/demopicasso/MainActivity.java
@@ -0,0 +1,26 @@
+package info.hienlth.demopicasso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgHinh;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //map control
+        imgHinh = (ImageView)findViewById(R.id.imgHinh);
+
+        //Đọc và hiển thị hình dùng Picasso
+        Picasso.get()
+                .load("http://hcmup.edu.vn/images/stories/site_18/mainsite/daotao.png")
+                .error(R.drawable.noimage)
+                .fit()
+                .into(imgHinh);
+    }
+}
